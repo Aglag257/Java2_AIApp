e65d0b6dad8a24446ad2c249afe1413865eea959
NickRagousis <t8210127@aueb.gr>
2023-12-07 16:52:03 +0200

    chargpt API key file added

--- app/src/main/java/gr/aueb/App.java
+++ app/src/main/java/gr/aueb/App.java
@@ -14,21 +14,28 @@ public class App
         
         Scanner sc = new Scanner(System.in);
         File f1 = new File("C:\\Users\\Nick\\api_keys\\tmdb_api_key.txt");
-        String apiKey = null;
+        File f2 = new File("C:\\Users\\Nick\\api_keys\\chat_gpt_key");
+        String tmdbApiKey = new String();
+        String chatgptApiKey = new String();
 
         try (BufferedReader br = new BufferedReader(new FileReader(f1))) {
-            apiKey = br.readLine();
+            tmdbApiKey = br.readLine();
         } catch (Exception e) {
             // TODO: handle exception
         }
 
+        try (BufferedReader br = new BufferedReader(new FileReader(f2))) {
+            chatgptApiKey = br.readLine();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
 
         System.out.println("Search for a movie. \n");
         String searchInput = sc.nextLine();        
-        ArrayList<Integer> ids = Movie.movieSearch(searchInput, apiKey);
+        ArrayList<Integer> ids = Movie.movieSearch(searchInput, tmdbApiKey);
         System.out.println("\nChoose your title. \n");
         int answer = sc.nextInt();
-        Movie.createMovie(ids.get(answer - 1), apiKey);
+        Movie.createMovie(ids.get(answer - 1), tmdbApiKey);
         sc.close();
 
     }
